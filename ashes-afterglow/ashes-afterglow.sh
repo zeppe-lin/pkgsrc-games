76ad2ef64d65b30aa4762f4fcc09de23be52a570
Alexandr Savca <alexandr.savca89@gmail.com>
2024-10-03 12:12:49 +0300

    ashes-afterglow: ashes-afterglow.sh: fix path

--- ashes-afterglow/ashes-afterglow.sh
+++ ashes-afterglow/ashes-afterglow.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec gzdoom -iwad /usr/share/doom/freedoom2.wad -file /usr/share/games/ashes-2063-2/*.pk3
+exec gzdoom -iwad /usr/share/doom/freedoom2.wad -file /usr/share/games/ashes-afterglow/*.pk3
