c3b3f89168a373f19476a0f799ad9b06417b5592
Alexandr Savca <alexandr.savca89@gmail.com>
2024-10-03 12:11:54 +0300

    ashes-2063: fix paths and names to correspond ashes-2063

--- ashes-2063/ashes-2063.sh
+++ ashes-2063/ashes-2063.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec gzdoom -iwad /usr/share/doom/freedoom2.wad -file /usr/share/games/ashes-2063-1/*.pk3
+exec gzdoom -iwad /usr/share/doom/freedoom2.wad -file /usr/share/games/ashes-2063/*.pk3
